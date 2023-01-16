62203056104bf8bcc11adad116f911775eb80297
tsuvi <admja11@gmail.com>
2023-01-16 20:42:47 +0900

    done for Object Types

--- arguments/client.js
+++ arguments/client.js
@@ -1,8 +1,15 @@
-var dice = 3;
-var sides = 6;
-var query = `query RollDice($dice: Int!, $sides: Int) {
-  rollDice(numDice: $dice, numSides: $sides)
+var numRolls = 3;
+var numSides = 6;
+// var query = `query RollDice($dice: Int!, $sides: Int) {
+//   rollDice(numDice: $dice, numSides: $sides)
+// }`;
+var query = `query getDie($numSides: Int, $numRolls: Int!) {
+  getDie(numSides: $numSides){
+    rollOnce
+    roll(numRolls: $numRolls)
+  }
 }`;
+
 
 fetch('http://localhost:4000/graphql', {
   method: 'POST',
@@ -12,7 +19,7 @@ fetch('http://localhost:4000/graphql', {
   },
   body: JSON.stringify({
     query,
-    variables: { dice, sides },
+    variables: { numRolls, numSides },
   })
 })
   .then(r => r.json())
--- arguments/server.js
+++ arguments/server.js
@@ -3,20 +3,40 @@ var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }  
   type Query {
-    rollDice(numDice: Int!, numSides: Int): [Int]
+      getDie(numSides: Int): RandomDie
   }
 `);
 
-var root = {
-  rollDice: ({numDice, numSides}) => {
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
     var output = [];
-    for (var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    for (var i = 0; i < numRolls; i++){
+      output.push(this.rollOnce());
     }
     return output;
   }
-};
+}
+
+var root = {
+  //destructing syntax -> getting a property from an object being passed as an argument to the getDie function
+  getDie: ({numSides}) => {
+    return new RandomDie(numSides || 6);
+  }
+}
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
