6fbcc231409fb56678fc38e0b76599804422c58e
tsuvi <admja11@gmail.com>
2023-01-17 23:58:15 +0900

    make env

---
+++ practice/arguments/client.js
@@ -0,0 +1,25 @@
+var numRolls = 3;
+var numSides = 6;
+// var query = `query RollDice($dice: Int!, $sides: Int) {
+//   rollDice(numDice: $dice, numSides: $sides)
+// }`;
+var query = `query getDie($numSides: Int, $numRolls: Int!) {
+  getDie(numSides: $numSides){
+    rollOnce
+    roll(numRolls: $numRolls)
+  }
+}`;
+
+fetch('http://localhost:4000/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+    variables: { numRolls, numSides },
+  })
+})
+  .then(r => r.json())
+  .then(data => console.log('data returned:', data));
