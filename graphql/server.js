00e70b23ebda2603396294d953da5d091a7abf83
tsuvi <admja11@gmail.com>
2023-01-19 20:55:42 +0900

    練習用の余計なファイル（graphql公式チュートリアル）を削除。expressに対するapollo server pluginを削除し、standaloneに変更。

--- graphql/server.js
+++ graphql/server.js
@@ -2,41 +2,27 @@
 //https://the-guild.dev/graphql/tools/docs/migration/migration-from-import
 //https://zenn.dev/eringiv3/books/a85174531fd56a/viewer/a8fab6
 
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import resolvers from './resolvers.js';
-import { join } from 'node:path'
-import { loadSchemaSync } from '@graphql-tools/load'
-import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
-import { addResolversToSchema } from '@graphql-tools/schema'
+import { ApolloServer } from "@apollo/server";
+import resolvers from "./resolvers.js";
+import { join } from "node:path";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { addResolversToSchema } from "@graphql-tools/schema";
+import { startStandaloneServer } from '@apollo/server/standalone';
 
-const app = express();
-
-// Set up Apollo Server
-const schema = loadSchemaSync(join(process.cwd(), 'schema.graphql'), {
-   loaders: [new GraphQLFileLoader()] 
+async function startApolloServer() {
+  
+  // Set up Apollo Server
+  const schema = loadSchemaSync(join(process.cwd(), "schema.graphql"), {
+    loaders: [new GraphQLFileLoader()],
   });
-const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
-const httpServer = http.createServer(app);
-const server = new ApolloServer({
-  schema: schemaWithResolvers,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-});
-await server.start();
+  const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
 
-// Apply express middleware for Apollo Server
-app.use(
-  cors(),
-  bodyParser.json(),
-  expressMiddleware(server),
-);
+  const server = new ApolloServer({
+    schema: schemaWithResolvers,
+  });
+  const { url } = await startStandaloneServer(server);
+  console.log(`🚀 Server ready at ${url}`);
 
-// Start the server
-await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log(`🚀 Server ready at http://localhost:4000`);
-
+}
+startApolloServer();
